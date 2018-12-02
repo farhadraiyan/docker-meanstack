adf2bb1cffedf9d7381d64236b4d66039ca69274
farhad <farhadtex2011@gmail.com>
2018-12-02 03:14:02 -0500

    added crud operations with routes to customers

--- routes/customerroutes.js
+++ routes/customerroutes.js
@@ -9,4 +9,57 @@ router.get('/customers', (req, res, next) => {
     })
 });
 
+router.post('/addcustomers', (req, res, next) => {
+    let newcust = new Customer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        address: req.body.address,
+        city: req.body.city,
+        phone: req.body.phone,
+        status: req.body.status
+
+
+    })
+
+    newcust.save((err, customer) => {
+        if (err) { res.json("contact not saved") }
+
+        else {
+            res.json("contact saved")
+        }
+
+    })
+
+
+})
+
+router.put("/updatecustomer/:id", (req, res, next) => {
+    let newCust = new Customer({
+        _id: req.params.id,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        address: req.body.address,
+        city: req.body.city,
+        phone: req.body.phone,
+        status: req.body.status
+    });
+    Customer.findOneAndUpdate({ _id: req.params.id },newCust, {"new": true} ,(err, customer) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(customer)
+
+    });
+});
+router.delete("/customer/:id", (req, res, next) => {
+    Customer.remove({ _id: req.params.id }, (err, result) => {
+        if (err) {
+            res.json(err)
+        }
+        else {
+            res.json(result)
+        }
+    })
+});
+
 module.exports = router;
