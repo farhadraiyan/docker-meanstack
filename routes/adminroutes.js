96a0b1fb4112fa9fe7ca52b64e08d3c0d4d36c6f
farhad <farhadtex2011@gmail.com>
2018-12-02 03:29:00 -0500

    CRUD operation is done for admin with routes setup in backend

--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -11,5 +11,50 @@ router.get('/login', (req, res, next) => {
         res.json(admins);
     })
 });
+router.post('/register', (req, res, next) => {
+    let newAdmin = new Admin({
+        userName: req.body.userName,
+        password: req.body.password,
+
+
+    })
+
+    newAdmin.save((err, admin) => {
+        if (err) { res.json("contact not saved") }
+
+        else {
+            res.json("contact saved")
+        }
+
+    })
+
+
+})
+
+router.put("/updateadmin/:id", (req, res, next) => {
+    let newAdmin = new Admin({
+        _id: req.params.id,
+        userName: req.body.userName,
+        password: req.body.password
+
+    });
+    Admin.findOneAndUpdate({ _id: req.params.id },newAdmin, {"new": true} ,(err, admin) => {
+        if (err) {
+            res.send(err)
+        }
+        res.send(admin)
+
+    });
+});
+router.delete("/admin/:id", (req, res, next) => {
+    Admin.remove({ _id: req.params.id }, (err, result) => {
+        if (err) {
+            res.json(err)
+        }
+        else {
+            res.json(result)
+        }
+    })
+});
 
 module.exports = router;
